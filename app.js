092e74aed912f8f35d9689e4b485114f6d631b1b
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-11-09 15:39:39 +0100

    GET and POST products

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const uniqid = require('uniqid');
+const cors = require('cors');
+
+const productList = [
+  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
+  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
+  { id: 3, name: 'produit 3', price: 20, quantity: 5 },
+];
 
 const app = express();
 
-const items = [
-  { id: 1, name: 'item1' },
-  { id: 2, name: 'item2' },
-];
+app.use(express.json());
+app.use(cors());
 
-app.get('/myroute', (req, res) => {
-  console.log('handling /myroute');
-  res.send(items);
+app.get('/products', (request, response) => {
+  response.send(productList);
+});
+
+app.post('/products', (request, response) => {
+  const { name, price } = request.body;
+  const newProduct = { name, price, quantity: 1, id: uniqid() };
+  productList.push(newProduct);
+  response.send(newProduct);
 });
 
 app.listen(5000, () => console.log('server listening on port 5000'));
