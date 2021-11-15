1e9503ce0274c1d76fbbc9ae16d7c162d1761064
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-11-15 15:25:02 +0100

    init sql connection

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const uniqid = require('uniqid');
-const cors = require('cors');
+const connection = require('./db');
 
-const productList = [
-  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
-  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
-  { id: 3, name: 'produit 3', price: 20, quantity: 5 },
-];
-
+const serverPort = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting to db');
+  } else {
+    console.log('connected to db');
+  }
+});
 
+app.listen(serverPort);
+
+/*
 app.get('/products', (request, response) => {
   response.send(productList);
 });
@@ -23,5 +25,4 @@ app.post('/products', (request, response) => {
   productList.push(newProduct);
   response.send(newProduct);
 });
-
-app.listen(5000, () => console.log('server listening on port 5000'));
+*/
